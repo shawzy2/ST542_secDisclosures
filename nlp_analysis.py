971b8baa118c79a24e474eef88bdc51e2fb418ea
ironmarcus <ironshawn@gmail.com>
2022-03-03 22:44:48 -0500

    Got the nlp analysis working. Going to add fog index

---
+++ nlp_analysis.py
@@ -0,0 +1,52 @@
+import os
+import json
+import pandas as pd
+import nltk
+import pprint
+from nltk.tokenize import word_tokenize
+
+def lexical_diversity(text):
+    return len(set(text)) / len(text)
+def di_stat(d):
+    dictionary_data = d
+    #1. how many words in di
+    #2. does it have a table
+    #3. Lexical diversity (LD) is considered to be an important indicator of how complex and difficult to read a text is.
+
+    summary_list = []
+    value2= []
+    all_cik_v2 = {}
+    for key,value in dictionary_data.items():
+        # key is "allFilings2021_part1.json"
+        # value is a dictionary
+        all_cik = {}
+        new_dict = {}
+        for sub_key, sub_value in value.items():
+            # sub_key is now cik
+            # sub_value is a di_section that is in a list form
+            new_value = ",".join(value)
+            new_dict[key] = new_value
+            for sub_key2, sub_value2 in new_dict.items():
+                cik_di = {}
+                summary_dict = {}
+                word_list = word_tokenize(sub_value2)
+                len_of_word_list = len(word_list)
+                reading_difficulty = lexical_diversity(sub_value2)
+                if "table" in value:# This might not be that accurate
+                    table_bool = 1
+                else:
+                    table_bool = 0
+
+                summary_dict["elements_num"] = len_of_word_list
+                summary_dict["reading_score"] = reading_difficulty # the higher it is, the more complex
+                summary_dict["table_present"] = table_bool
+                #pprint.pprint(summary_dict)
+                #print(summary_dict)
+                
+                cik_di[sub_key] = summary_dict
+                print(cik_di)
+    #     all_cik.update(cik_di)
+    # all_cik_v2.update(all_cik)
+    #pprint.pprint(all_cik_v2)
+
+    #return summary_dict
--- sec_analysis.py
+++ sec_analysis.py
@@ -2,17 +2,31 @@ import os
 import json
 import pandas as pd
 import nltk
+import nlp_analysis
 
 
-# This code should combine all of the json files
-data = {}
+di_stat = nlp_analysis.di_stat
+# # This code should combine all of the json files
+# data = {}
 
-files = ["part1dioutput.json","dioutput.json","allFilings2021_part8.json"]
-# Merge all files
-#for file in os.listdir("../ST542_secDisclosures/"):
-for file in files:
-    new_name = f"../ST542_secDisclosures/yescik/di/{file}"
-    data.update(json.load(open(new_name, encoding = 'utf-8')))
+# files = ["part1dioutput.json","dioutput.json","allFilings2021_part8.json"]
+# # Merge all files
+# for file in files:
+#     new_name = f"../ST542_secDisclosures/{file}"
+#     data.update(json.load(open(new_name, encoding = 'utf-8')))
 
-with open(f"../ST542_secDisclosures/di.json","w", encoding='utf8') as new_content:
-    new_content.write(json.dump(data, new_content,ensure_ascii=False, indent=4)) 
+# with open(f"../ST542_secDisclosures/di.json","w", encoding='utf8') as new_content:
+#     new_content.write(json.dump(data, new_content,ensure_ascii=False, indent=4)) 
+
+
+# NLP Analysis
+di_file = "di.json"
+#if di_file  in os.listdir("../ST542_secDisclosures/"):
+    #print("TRUE")
+di_file_dic = {}
+new_di_file = f"../ST542_secDisclosures/{di_file}"
+with open(new_di_file, encoding='utf-8') as content:
+    json_data = json.load(content)
+    di_stat_data = di_stat(json_data)
+    di_stat_data
+    # di_file_dic[di_file] = di_stat_data
